832287635c0ae1c7370735864256343700ebea92
thayninh <thayninh@gmail.com>
2017-12-03 20:16:48 +0800

    backup

---
+++ index.js
@@ -0,0 +1,42 @@
+/* ===================
+   Import Node Modules
+=================== */
+const express = require('express'); // Fast, unopinionated, minimalist web framework for node.
+const app = express(); // Initiate Express Application
+const router = express.Router(); // Creates a new router object.
+const mongoose = require('mongoose'); // Node Tool for MongoDB
+mongoose.Promise = global.Promise;
+const config = require('./config/database'); // Mongoose Config
+const path = require('path'); // NodeJS Package for file paths
+const bodyParser = require('body-parser'); // Parse incoming request bodies in a middleware before your handlers, available under the req.body property.
+const cors = require('cors'); // CORS is a node.js package for providing a Connect/Express middleware that can be used to enable CORS with various options.
+const port = 8080; 
+
+// Database Connection
+mongoose.connect(config.uri, {
+  useMongoClient: true,
+}, (err) => {
+  // Check if database was able to connect
+  if (err) {
+    console.log('Could NOT connect to database: ', err); // Return error message
+  } else {
+    console.log('Connected to ' + config.db); // Return success message
+  }
+});
+
+// Middleware
+app.use(cors({ origin: 'http://localhost:4200' })); // Allows cross origin in development only
+app.use(bodyParser.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
+app.use(bodyParser.json()); // parse application/json
+app.use(express.static(__dirname + '/public')); // Provide static directory for frontend
+
+// Connect server to Angular 2 Index.html
+app.get('*', (req, res) => {
+  //res.sendFile(path.join(__dirname + '/public/index.html'));
+  res.send("OK")
+});
+
+// Start Server: Listen on port 8080
+app.listen(port, () => {
+  console.log('Listening on port ' + port);
+});
