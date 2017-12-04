90447701944b55d129829f008479e4ce29022e84
thayninh <thayninh@gmail.com>
2017-12-04 23:53:13 +0800

    backup

--- client/src/app/map/map.component.ts
+++ client/src/app/map/map.component.ts
@@ -15,21 +15,36 @@ export class MapComponent implements OnInit {
   public map: any;
   
   constructor() { 
+    //Basemap
     var osm_layer: any = new ol.layer.Tile({
       source: new ol.source.OSM()
     });
     
+    //Scale line
+    var scaleLineControl = new ol.control.ScaleLine();
+    
+    //Zoom slider
+    var zoomslider = new ol.control.ZoomSlider();
+    
     // note that the target cannot be set here!
     this.map = new ol.Map({
+      controls: ol.control.defaults({
+        attributionOptions: ({
+          collapsible: false
+        })
+      }).extend([
+        scaleLineControl
+      ]),
       layers: [osm_layer],
       view: new ol.View({
-      center: ol.proj.transform([0,0], 'EPSG:4326', 'EPSG:3857'),
-      zoom: 2
+        center: ol.proj.transform([108.14006321133, 16.027406863868], 'EPSG:4326', 'EPSG:3857'),
+        zoom: 5.5
       })
   });
+  this.map.addControl(zoomslider);
   }
   
-  // After view init the map target can be set!
+  // After view init, the map target can be set!
   ngAfterViewInit() {
     this.map.setTarget(this.mapElement.nativeElement.id);
   }
--- index.js
+++ index.js
@@ -3,14 +3,14 @@
 =================== */
 const express = require('express'); // Fast, unopinionated, minimalist web framework for node.
 const app = express(); // Initiate Express Application
-const router = express.Router(); // Creates a new router object.
 const mongoose = require('mongoose'); // Node Tool for MongoDB
 mongoose.Promise = global.Promise;
 const config = require('./config/database'); // Mongoose Config
 const path = require('path'); // NodeJS Package for file paths
 const bodyParser = require('body-parser'); // Parse incoming request bodies in a middleware before your handlers, available under the req.body property.
 const cors = require('cors'); // CORS is a node.js package for providing a Connect/Express middleware that can be used to enable CORS with various options.
-const port = 8080; 
+const port = 8080;
+const route = require('./routes/route'); 
 
 // Database Connection
 mongoose.connect(config.uri, {
@@ -25,15 +25,17 @@ mongoose.connect(config.uri, {
 });
 
 // Middleware
-app.use(cors({ origin: 'http://localhost:4200' })); // Allows cross origin in development only
+app.use(cors()); // Allows cross origin in development only
 app.use(bodyParser.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); // parse application/json
 app.use(express.static(__dirname + '/public')); // Provide static directory for frontend
 
+//routes
+app.use('/api', route);
+
 // Connect server to Angular 2 Index.html
-app.get('*', (req, res) => {
-  //res.sendFile(path.join(__dirname + '/public/index.html'));
-  res.send("OK")
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
 // Start Server: Listen on port 8080
